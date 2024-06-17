4ba39f6e46a758f8978b5ae89ded412a8ed3ab8b
Manav-NTT <manav12.gupta@nttdata.com>
2024-06-17 18:01:08 +0530

    added bootstrap and functionality to add item to todolist

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -4,8 +4,11 @@ package com.in28minutes.springboot.myfirstwebapp.todo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -22,5 +25,16 @@ public class TodoController {
         List<Todo> todos = todoService.findByUsername("in28minutes");
         modelMap.addAttribute("todos", todos);
         return "listTodos";
+    }
+
+    @RequestMapping(value = "add-todo", method = RequestMethod.GET)
+    public String showNewTodoPage() {
+        return "todo";
+    }
+
+    @RequestMapping(value = "add-todo", method = RequestMethod.POST)
+    public String addNewTodoPage(@RequestParam String description, ModelMap modelMap) {
+        todoService.addTodo((String) modelMap.get("name"), description, LocalDate.now().plusYears(1), false);
+        return "redirect:list-todos";
     }
 }
